308e5ba399992175e5f2fdbbb42f6cdd4c01e9b7
Steven Zhang <steven.shutao@gmail.com>
2016-12-30 10:45:17 +0800

    upload OK

--- sub.py
+++ sub.py
@@ -5,9 +5,9 @@
 
 
 from os.path import join
-from subprocess import run
+from subprocess import run, TimeoutExpired, CalledProcessError
 from subproc.utility import logger, get_current_path, get_winscp_directory, \
-							get_winscp_log_directory
+							get_winscp_log_directory, dt_string_to_datetime
 
 
 # The simplest working example to call WinSCP to download a file from a public
@@ -18,10 +18,97 @@ from subproc.utility import logger, get_current_path, get_winscp_directory, \
 # 
 # The way to invoke a winscp.com program is from the bat file: sftp-upload.com
 #
-args = [join(get_winscp_directory(), 'WinSCP.com'), \
-		'/script={0}'.format(join(get_current_path(), 'run-sftp.txt')), \
-		'/log={0}'.format(join(get_winscp_log_directory(), 'upload.log'))]
+def get_file():
+	args = [join(get_winscp_directory(), 'WinSCP.com'), \
+			'/script={0}'.format(join(get_current_path(), 'run-sftp.txt')), \
+			'/log={0}'.format(join(get_winscp_log_directory(), 'winscp.log'))]
 
-result = run(args)
-# result = run(args, timeout=2)
-print(result)
+	result = run(args)
+	print(result)
+
+
+
+def get_file_error():
+	"""
+	Get 3 files:
+	file1 (good)
+	file2 (does not exist)
+	file3 (good)
+
+	The observation: file1 get successfully, file2 cause error, sftp session
+	then stops, so file3 not downloaded.
+	"""
+	args = [join(get_winscp_directory(), 'WinSCP.com'), \
+			'/script={0}'.format(join(get_current_path(), 'run-sftp-error.txt')), \
+			'/log={0}'.format(join(get_winscp_log_directory(), 'winscp.log'))]
+
+	result = run(args, check=True)	# check for return code, if it is non-zero,
+									# then raise CalledProcessError
+	print(result)	# never executed due to exception raised, if check=False (default),
+					# then we can see the result
+
+
+
+def get_file_timeout():
+	"""
+	Get 2 files, but with a timeout of 3 seconds.
+	"""
+	args = [join(get_winscp_directory(), 'WinSCP.com'), \
+			'/script={0}'.format(join(get_current_path(), 'run-sftp-2files.txt')), \
+			'/log={0}'.format(join(get_winscp_log_directory(), 'winscp.log'))]
+
+	result = run(args, timeout=25, check=True)	# check for time out and return
+												# code.
+	print(result)	# never executed due to exception raised
+
+
+
+def read_log(winscp_log):
+	"""
+	Look for successful transfer records in the winscp logfile, then report
+	which files are successfully transferred, and the date and time those
+	transfers are completed.
+
+	The successful transfer records are in the following format (get and put)
+
+	> 2016-12-29 17:20:40.652 Transfer done: '<file full path>' [xxxx]
+
+	The starting symbol can be '>', '<', '.', '!', depending on the type of
+	the record.
+
+	If it is a get, then 'file full path' will be the remote directory's
+	file path. If it is a put, then 'file full path' will be the local
+	directory's file path.
+	"""
+	result = {}
+	with open(winscp_log) as f:
+		for line in f:
+			tokens = line.split()
+			# print(tokens)
+			if len(tokens) < 6:
+				continue
+
+			if tokens[3] == 'Transfer' and tokens[4] == 'done:':
+				dt = dt_string_to_datetime(tokens[1] + ' ' + tokens[2].split('.')[0])
+				result[tokens[5][1:-1]] = dt
+
+	return result
+
+
+
+if __name__ == '__main__':
+	try:
+		# get_file()
+		get_file_timeout()
+		# get_file_error()
+		pass
+	except TimeoutExpired as e:
+		print('process timeout within {0} seconds'.format(e.timeout))
+	except CalledProcessError as e:
+		print('process error code {0}'.format(e.returncode))
+		print('error message stdout: {0}'.format(e.stdout))	# does not get anything
+		print('error message stderr: {0}'.format(e.stderr))	# does not get anything
+	finally:
+		print(read_log(join(get_winscp_log_directory(), 'winscp.log')))
+
+	print('working OK')
--- utility.py
+++ utility.py
@@ -6,6 +6,7 @@
 
 import configparser, os
 from config_logging.file_logger import get_file_logger
+from datetime import datetime
 
 
 
@@ -79,4 +80,34 @@ def get_winscp_directory():
 
 def get_winscp_log_directory():
 	global config
-	return config['directory']['winscp_log']
+	if config['directory']['winscp_log'] == '':
+		return get_current_path()
+	else:
+		return config['directory']['winscp_log']
+
+
+
+def dt_string_to_datetime(dt_string):
+	"""
+	Copied from reconciliation_helper.record.py.
+
+	convert a string in 'yyyy-mm-dd hh:mm:ss' format to a datetime
+	object.
+	"""
+	dt_token = dt_string.split()[0]
+	tm_token = dt_string.split()[1]
+	year, month, day = parse_string(dt_token, '-')
+	hour, minute, second = parse_string(tm_token, ':')
+	return datetime(year, month, day, hour=hour, minute=minute, second=second)
+
+
+
+def parse_string(a_string, separator):
+	"""
+	Copied from reconciliation_helper.record.py.
+
+	Convert string of form 'yyyy-mm-dd' or 'hh-mm-ss' into three
+	integers.
+	"""
+	a_list = a_string.split(separator)
+	return int(a_list[0]), int(a_list[1]), int(a_list[2])
